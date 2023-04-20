9d41e82509081a6721894394eb5b1ca4ad375747
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-04-19 23:26:15 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Tracey Buentello and is{" "}
+        <a
+          href="https://github.com/tleanne1/react-weather-app."
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
