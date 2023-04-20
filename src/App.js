b1f5a92e23a12c20d396c25a7a613d2fcf53d5c2
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-04-19 23:37:31 -0500

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Tracey Buentello and is{" "}
+        <a
+          href="https://github.com/tleanne1/react-weather-app."
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
