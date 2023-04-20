267d5075a5efe58d84a09171c754596e069a5aa9
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-04-19 23:36:09 -0500

    Removed GitHub URL

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <footer>
-        This project was coded by Tracey Buentello and is{" "}
-        <a
-          href="https://github.com/tleanne1/react-weather-app."
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
     </div>
   );
 }
