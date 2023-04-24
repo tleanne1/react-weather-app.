aa65ef67f41c5fdf17275cb58a2b5f16d102da86
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-04-23 21:44:40 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Tracey Buentello and is{" "}
-        <a
-          href="https://github.com/tleanne1/react-weather-app."
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Tracey Buentello and is{" "}
+          <a
+            href="https://github.com/tleanne1/react-weather-app."
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
